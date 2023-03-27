f4e4553bd1728a6d3e9cbb983d19f86a9f13be84
Lee-SeungHoon <vheh0901@gmail.com>
2023-03-27 16:40:46 +0900

    git init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Demo from "./pages/Demo";
+import Footer from "./pages/Footer";
+import Header from "./pages/Header";
+import Introduction from "./pages/Introduction";
+import Speciality from "./pages/Speciality";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Introduction />
+      <Demo />
+      <Speciality />
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Speciality.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Speciality = () => {
+  return <div>Speciality</div>;
+};
+
+export default Speciality;
